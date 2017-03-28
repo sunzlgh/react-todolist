92e2474977794e9fe51c6c251ef5b3c3b3b0cd92
wangyafang <wangyafang@sogou-inc.com>
2017-03-28 10:16:20 +0800

    完善ExtractTextPlugin兼容性

--- webpack.config.js
+++ webpack.config.js
@@ -16,49 +16,55 @@ module.exports = {
         path: BUILD_PATH,
         filename: 'bundle.js'
     },
-    //启动dev source map，出错以后就会采用source-map的形式直接显示你出错代码的位置。
+    //启动dev source map，出错以后就会采用source-map的形式直接显示你出错代码的位置
     devtool: 'eval-source-map',
     devServer: {
-    	contentBase: "./",  // 本地服务器所加载的页面所在的目录
+        contentBase: "./",  // 本地服务器所加载的页面所在的目录
         historyApiFallback: true,  // 不跳转
         inline: true  // 实时刷新
     },
     module: {
         rules: [{
             test: /\.(js|jsx)?$/,
-            loader: 'babel-loader',
-            include: APP_PATH,
-            query: {
-                //添加两个presets 使用这两种presets处理js或者jsx文件
-                presets: ['es2015', 'react']
-            }
-        }, 
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        [
+                            'es2015', 
+                            { "modules": false }
+                        ], 
+                        'react'
+                    ]
+                }
+            }]
+        },
         {
-            test: /\.scss$/,
-            loaders: ['style-loader', 'css-loader', 'sass-loader']
-             
-        }
-        // {
-        //     test: /\.scss$/,
-        //     use: ExtractTextPlugin.extract({
-	    //       fallback: "style-loader",
-	    //       use: ['css-loader', 'sass-loader']
-	    //     })
-        // }
-        ]
+            test: /\.(sass|scss)$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: ['css-loader', 'sass-loader']
+            })
+        }]
     },
     plugins: [
         //使用uglifyJs压缩js代码
-        new webpack.optimize.UglifyJsPlugin({ minimize: true }),
+        new webpack.optimize.UglifyJsPlugin({ 
+            minimize: true,
+            comments: false,        //去掉注释
+            compress: {
+                warnings: false    //忽略警告,要不然会有一大堆的黄色字体出现……
+            }
+        }),
         new HtmlwebpackPlugin({
             template: './index.html'   // 模版文件
         }),
         new webpack.HotModuleReplacementPlugin(), // 热加载插件
-        // new ExtractTextPlugin('./styles/index.scss')  //分离css文件,注意该插件由于和webpack2不兼容，需要指定版本
+        new ExtractTextPlugin('styles.css'),  //分离css文件,注意该插件由于和webpack2不兼容，需要指定版本
         //把入口文件里面的数组打包成verdors.js
-        // new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+        // new webpack.optimize.CommonsChunkPlugin({name: 'vendors', filename: 'vendors.js'})
     ],
-    //自动扩展文件后缀名，意味着我们require模块可以省略不写后缀名
+    //自动扩展文件后缀名，意味着require模块可以省略不写后缀名
     resolve: {
         extensions: ['.js', '.jsx']
     }
